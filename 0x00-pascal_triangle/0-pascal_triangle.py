3fdc9fb9c510b58bdf11e3863f3537eb1f228699
Nasef161995 <mnasef905@gmail.com>
2024-04-12 00:00:11 +0300

    task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Pascale triangle module for the Problem to solve"""
 
+
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
